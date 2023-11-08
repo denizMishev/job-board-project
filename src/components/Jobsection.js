696c2d263c171ce671bb7912b032fc5dd7febb9b
Mishev Deniz <denizmishev@yahoo.com>
2023-11-08 21:52:20 +0200

    added dynamic loading of jobs in job section with all their properties

--- src/components/Jobsection.js
+++ src/components/Jobsection.js
@@ -1,17 +1,32 @@
 import { Jobcard } from "./Jobcard";
 
+import { jobsData } from "../data/data";
+
 export function Jobsection() {
   return (
     <main>
       <section className="landingpage-jobs | bg-neutral-200">
         <div className="jobs-container | container">
           <div className="jobs-grid">
+            {/* <Jobcard></Jobcard>
             <Jobcard></Jobcard>
             <Jobcard></Jobcard>
             <Jobcard></Jobcard>
             <Jobcard></Jobcard>
-            <Jobcard></Jobcard>
-            <Jobcard></Jobcard>
+            <Jobcard></Jobcard> */}
+            {jobsData.map((jobData) => (
+              <Jobcard
+                key={jobData.id}
+                jobId={jobData.id}
+                company={jobData.company}
+                logoImage={jobData.logo}
+                logoBackgroundColor={jobData.logoBackground}
+                position={jobData.position}
+                postedAt={jobData.postedAt}
+                workingTime={jobData.contract}
+                location={jobData.location}
+              />
+            ))}
           </div>
           <button className="jobs-load-more-button | button">Load More</button>
         </div>
