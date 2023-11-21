e149eac7a11a15eaf9f0a275b57023e198ca1a0e
Mishev Deniz <denizmishev@yahoo.com>
2023-11-21 16:26:15 +0200

    added data import script, imported data in project's firestore db

---
+++ src/data/importDataScript.js
@@ -0,0 +1,37 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("deleted");
+
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} catch (error) {
+  console.error("firebase error", error);
+  process.exit(1);
+}
+
+const db = admin.firestore();
+
+const jsonData = require("./data.json");
+
+async function importData() {
+  const batch = db.batch();
+
+  try {
+    for (const key in jsonData) {
+      const docRef = db.collection("jobs").doc();
+      const dataWithoutId = { ...jsonData[key] };
+      delete dataWithoutId.id;
+
+      batch.set(docRef, dataWithoutId);
+    }
+
+    await batch.commit();
+    console.log("import successful");
+  } catch (error) {
+    console.error("import failed:", error);
+    process.exit(1);
+  }
+}
+
+importData();
