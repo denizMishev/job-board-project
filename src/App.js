871d42ef1a64ce462dca44e22487301e81d46237
Mishev Deniz <denizmishev@yahoo.com>
2023-11-25 11:01:14 +0200

    added Routes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Jobsection } from "./components/Jobsection";
 import { SearchProvider } from "./Context/SearchContext";
+import { JobDetails } from "./components/JobDetails";
 
 function App() {
   return (
     <SearchProvider>
       <div className="App">
         <Header />
-        <Jobsection />
+        <Routes>
+          <Route path="/" element={<Jobsection />}></Route>
+          <Route path="/jobs/:jobId" element={<JobDetails />}></Route>
+          {/* <Jobsection /> */}
+        </Routes>
       </div>
     </SearchProvider>
   );
