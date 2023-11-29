524e7e84c9b9545f515dde7ad107a0f4422fbce0
Mishev Deniz <denizmishev@yahoo.com>
2023-11-29 17:45:19 +0200

    updated loading spinner to be a rotator one instead of a bar one

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,22 +1,16 @@
 import React from "react";
 import { css } from "@emotion/react";
-import { BarLoader } from "react-spinners";
+import { RotateLoader } from "react-spinners";
 
 const override = css`
   display: block;
   margin: 0 auto;
-  border-color: red;
 `;
 
-export function LoadingSpinner({ loading }) {
+export function LoadingSpinner() {
   return (
     <div className="loading-spinner-container">
-      <BarLoader
-        css={override}
-        loading={loading}
-        color={"#36D7B7"} // You can customize the color
-        size={150} // You can customize the size
-      />
+      <RotateLoader css={override} color={"#36D7B7"} size={20} />
     </div>
   );
 }
