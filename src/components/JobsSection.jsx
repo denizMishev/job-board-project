765cdffab29f36426dc7d137af8d79710ee5c2af
Mishev Deniz <denizmishev@yahoo.com>
2024-12-26 17:26:39 +0200

    updated loading spinner to .tsx, removed emotion css prop for style prop

--- src/components/JobsSection.jsx
+++ src/components/JobsSection.jsx
@@ -7,7 +7,7 @@ import { getJobs } from "../api/getJobs.js";
 
 import { useSearch } from "../context/SearchContext.jsx";
 
-import { LoadingSpinner } from "./LoadingSpinner.jsx";
+import { LoadingSpinner } from "./LoadingSpinner";
 import { NoSearchResults } from "./NoSearchResults.jsx";
 import { PageSelector } from "./PageSelector.jsx";
 
